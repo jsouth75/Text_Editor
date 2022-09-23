7b51b28fa4130de2292371aad50c1c2e48aac02a
Jason South <jason.south@me.com>
2022-09-22 22:28:59 -0500

    updated src files

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,27 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  const jateDb = await openDB("jate", 1);
+  const transaction = jateDb.transaction("jate", "readwrite");
+  const store = transaction.objectStore("jate");
+  const result = await request;
+  console.log("Data saved", result);
+
+  // console.error('putDb not implemented');
+}
+
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  const jateDb = await openDB("jate", 1);
+  const transaction = jateDb.transaction("jate", "readonly");
+  const store = transaction.objectStore("jate");
+  const request = store.getAll();
+  const result = await request;
+  console.log(result);
+
+  // console.error('getDb not implemented');
+}
 
 initdb();
